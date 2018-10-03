ea641f8677ad5ae88aab757f30cb2ec52494d8cd
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2018-10-02 21:50:40 -0500

    got basic radio buttons (not pure CSS)

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 import os
 
-from flask import Flask, g, render_template, url_for
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 import psycopg2
+
+import db
 
 app = Flask(__name__)
 
-def connect_db():
-    """Connects to the specific database."""
-    return psycopg2.connect(os.environ.get('DB_DSN'))
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'pg_db'):
-        g.pg_db = connect_db()
-    return g.pg_db
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'db'):
-        g.pg_db.close()
+@app.before_first_request
+def initialize():
+    db.setup()
 
 @app.route('/')
 def index():
---
+++ db.py
@@ -0,0 +1,46 @@
+""" database access - from in-class 9/26
+docs:
+* http://initd.org/psycopg/docs/
+* http://initd.org/psycopg/docs/pool.html
+* http://initd.org/psycopg/docs/extras.html#dictionary-like-cursor
+"""
+
+from contextlib import contextmanager
+import logging
+import os
+
+from flask import current_app, g
+
+import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import DictCursor
+
+pool = None
+
+def setup():
+    global pool
+    DATABASE_URL = os.environ['DATABASE_URL']
+    current_app.logger.info(f"creating db connection pool")
+    pool = ThreadedConnectionPool(1, 4, dsn=DATABASE_URL, sslmode='require')
+
+
+@contextmanager
+def get_db_connection():
+    try:
+        connection = pool.getconn()
+        yield connection
+    finally:
+        pool.putconn(connection)
+
+
+@contextmanager
+def get_db_cursor(commit=False):
+    with get_db_connection() as connection:
+      cursor = connection.cursor(cursor_factory=DictCursor)
+      # cursor = connection.cursor()
+      try:
+          yield cursor
+          if commit:
+              connection.commit()
+      finally:
+          cursor.close()
