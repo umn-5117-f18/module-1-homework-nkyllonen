0e70f6432937373e8f0e33d74a27b98d0e8649f4
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2018-10-03 21:39:07 -0500

    checkbox and textarea and started DB org

--- schema.sql
+++ schema.sql
@@ -1,16 +1,12 @@
 
 drop table person;
-drop table skill;
 
 create table person(
   person_id SERIAL PRIMARY KEY,
   name varchar(255)
-);
-
-create table skill(
-  skill_id SERIAL PRIMARY KEY,
-  person_id int references person,
-  skill varchar(255)
+  color varchar(255)
+  music varchar(255)
+  breed varchar(255)
 );
 
 -- insert into person (name) values ('max');
